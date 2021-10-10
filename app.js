66aa8bb26d74413ea76166ab37f41c7e2d76904c
SzabiSch <szabiweb21@gmail.com>
2021-10-10 23:48:15 +0200

    Logic eingetipp, aber Wertübergabe Fehler und ??? getestet bei JSbin dass Show/Hidden Passport funktionert, aber in Browser nicht mehr

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 //Password Field inkl Hidden/show Button
 
 function myFunction() {
+  const btnShowHidden = document.getElementById("#show-hidden");
   var pass1 = document.getElementById("myInput1");
   var pass2 = document.getElementById("myInput2");
   if (pass1.type === "password" || pass2.type === "password") {
     pass1.type = "text";
     pass2.type = "text";
-
-    //label.parentElement.classList.remove("ShowPassword");
-    //label.parentElement.classList.add("HiddenPassword");
+    btnShowHidden.innerText = "Hidden Password";
   } else {
     pass1.type = "password";
     pass2.type = "password";
-    //label.parentElement.classList.add("ShowPassword");
-    //label.parentElement.classList.remove("HiddenPassword");
+    btnShowHidden.innerText = "Show Password";
   }
 }
 
@@ -24,18 +22,70 @@ const inputPasswordSecond = document.querySelector("#myInput2");
 inputPasswordSecond.addEventListener("keyup", writeThePass);
 
 function writeThePass() {
-  // console.log("Pass first: " + inputPasswordFirst.value);
-  //console.log("second first: " + inputPasswordSecond.value);
+  console.log("Pass first: " + inputPasswordFirst.value);
+  console.log("Pass second: " + inputPasswordSecond.value);
 }
 
-function passequal() {
-  if (inputPasswordFirst.value === inputPasswordSecond.value) {
-    console.log(inputPasswordSecond.value);
-    console.log("fast fertig");
-    equal - content - yes.parentElement.classList.add("equal-content-yes");
-    equal - content - no.parentElement.classList.remove("equal-content-no");
+Let controlContinue = 0;
+
+myInput2.addEventListener("keyup", controlEqual());
+
+function controlEqual() {
+  if (inputPasswordFirst.value == inputPasswordSecond.value) {
+    console.log("Hollee");
+    console.log(inputPasswordFirst.value);
+controlContinue++; 
+  }
+}
+
+function includeLowerCase() {
+  const contLow = document.getElementById("lower-content");
+  var charLow = /[a-z]/g;
+  const lookingLow = inputPasswordSecond.search(charLow);
+  if (controlContinue === 1 && lookingLow !== -1) {
+    
+      contLow.innerText = "✅";
+    } else {
+      contLow.innerText = "❌";
+    } 
+  
+}
+
+function includeUpperCase() {
+  const contUpp = document.getElementById("upper-content");
+  var charUp = /[A-Z]/g;
+  const lookingUpp = inputPasswordSecond.search(charUp);
+  if (controlContinue === 1 && lookingUpp !== -1){
+  contUpp.innerText = "✅";}
+  else {
+  contUpp.innerText = "❌";
+} 
+}
+
+function includeNumber() {
+ 
+const contNumb = document.getElementById("number-content");
+  var charNumb = /[0-9]/g;
+  const lookingNumb = inputPasswordSecond.search(charNumb);
+  if (controlContinue === 1 && lookingNumb !== -1){
+  contNumb.innerText = "✅";}
+  else {
+  contNumb.innerText = "❌";
+} 
+
+
+
+
+
+
+
+
+function tenCharachter() {
+  const moreThanTen = document.getElementById("char-content");
+
+  if (inputPasswordFirst.length >= 10) {
+    moreThanTen.innerText = "✅";
   } else {
-    equal - content - no.parentElement.classList.remove("equal-content-no");
-    equal - content - yes.parentElement.classList.add("equal-content-yes");
+    moreThanTen.innerText = "❌";
   }
 }
